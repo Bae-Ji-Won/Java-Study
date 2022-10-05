4b2ba56dae0c7f38f4cc1f73344ecea4047b4ac7
Bae-Ji-Won <qowhxk@naver.com>
2022-10-05 09:36:55 +0900

    -

---
+++ git/Lion-Java/src/Date10_05/java_exercise_02.java
@@ -0,0 +1,15 @@
+package Date10_05;
+
+public class java_exercise_02 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int temp = a;
+
+        a = b;
+        b = temp;
+        System.out.printf("a:%d b:%d", a, b);
+    }
+}
