636f5f79133f39a06897429e9dff50e9edd4d738
qowl880 <qowhxk@naver.com>
2022-10-06 14:37:39 +0900

    [CodeUp] 1017번 문제

---
+++ git/Lion-Java/src/Date10_05/CodeUp/No_1017.java
@@ -0,0 +1,13 @@
+package Date10_05.CodeUp;
+
+import java.util.Scanner;
+
+public class No_1017 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        for(int i=0; i<3; i++){
+            System.out.print(num+" ");
+        }
+    }
+}
