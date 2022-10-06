b6d31f114c0a9302bbd26ba014893c78299b0857
qowl880 <qowhxk@naver.com>
2022-10-06 14:37:45 +0900

    [CodeUp] 1018번 문제

---
+++ git/Lion-Java/src/Date10_05/CodeUp/No_1018.java
@@ -0,0 +1,13 @@
+package Date10_05.CodeUp;
+
+import java.util.Scanner;
+
+public class No_1018 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.nextLine();
+
+        System.out.println(str);
+    }
+}
