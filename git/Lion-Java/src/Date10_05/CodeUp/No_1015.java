5b1f710d7dfc2930e661da25ac7789b6b3868b94
qowl880 <qowhxk@naver.com>
2022-10-06 13:59:51 +0900

    [CodeUp] 1015번 문제

---
+++ git/Lion-Java/src/Date10_05/CodeUp/No_1015.java
@@ -0,0 +1,14 @@
+package Date10_05.CodeUp;
+
+import java.util.Scanner;
+
+public class No_1015 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        float num = scan.nextFloat();
+
+        System.out.printf("%.2f",num);
+    }
+}
+
