6417b02b8476e22704787d625e7d80035b645d10
qowl880 <qowhxk@naver.com>
2022-10-06 13:59:37 +0900

    [CodeUp] 1012번 문제

---
+++ git/Lion-Java/src/Date10_05/CodeUp/No_1012.java
@@ -0,0 +1,12 @@
+package Date10_05.CodeUp;
+
+import java.util.Scanner;
+
+public class No_1012 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        float num = scan.nextFloat();
+
+        System.out.printf("%.6f", num);
+    }
+}
