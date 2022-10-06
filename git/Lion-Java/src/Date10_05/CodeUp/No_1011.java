723572a9b57a946af534f1a232f684e3932aaf9c
qowl880 <qowhxk@naver.com>
2022-10-06 13:59:32 +0900

    [CodeUp] 1011번 문제

---
+++ git/Lion-Java/src/Date10_05/CodeUp/No_1011.java
@@ -0,0 +1,11 @@
+package Date10_05.CodeUp;
+
+import java.util.Scanner;
+
+public class No_1011 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char x = scan.next().charAt(0);
+        System.out.println(x);
+    }
+}
