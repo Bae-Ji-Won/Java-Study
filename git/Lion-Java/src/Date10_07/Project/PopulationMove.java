b7668fc608571dbb08fb20f2cfe42d6142cdf9d3
qowl880 <qowhxk@naver.com>
2022-10-07 11:11:03 +0900

    File parse/Mapping

--- git/Lion-Java/src/Date10_07/Project/PopulationMove.java
+++ git/Lion-Java/src/Date10_07/Project/PopulationMove.java
@@ -5,6 +5,9 @@ package Date10_07.Project;
    3. 이사하기 전 도시와 이사하고 난 후 도시의 값만 가져온다
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PopulationMove {
     private int fromSido;
     private int toSido;
@@ -26,8 +29,31 @@ public class PopulationMove {
         return toSido;
     }
 
+    public String Mapping(int num){
+
+        Map<Integer,String> map = new HashMap<>();
+        map.put(11,"서울");
+        map.put(21,"부산");
+        map.put(22,"대구");
+        map.put(23,"인천");
+        map.put(24,"광주");
+        map.put(25,"대전");
+        map.put(26,"울산");
+        map.put(29,"세종");
+        map.put(31,"경기도");
+        map.put(32,"강원도");
+        map.put(33,"충북");
+        map.put(34,"충남");
+        map.put(35,"전북");
+        map.put(36,"전남");
+        map.put(37,"경북");
+        map.put(38,"경남");
+        map.put(39,"제주");
+
+        return map.get(num);
+    }
     @Override
     public String toString() {
-      return fromSido+"에서 "+toSido+"로 이사갔습니다.";
+      return Mapping(fromSido)+"에서 "+Mapping(toSido)+"로 이사갔습니다.";
     }
 }
