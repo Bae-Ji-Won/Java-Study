204a6a50d740a73d9f47a9a5a11b0c363e239fb7
qowl880 <qowhxk@naver.com>
2022-10-07 09:03:56 +0900

    [CodeUp] 1021번 문제

---
+++ git/Lion-Java/src/Date10_06/CodeUp/No_1021.java
@@ -0,0 +1,15 @@
+package Date10_06.CodeUp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_1021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = new String[51];
+        str[0] = bf.readLine();
+
+        System.out.println(str[0]);
+    }
+}
