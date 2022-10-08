6fe28f3af9ecb94d44a789527284d610cbe2641b
qowl880 <qowhxk@naver.com>
2022-10-08 16:03:28 +0900

    [CodeUp] 1030번 문제

---
+++ git/Lion-Java/src/Date10_06/CodeUp/No_1030.java
@@ -0,0 +1,16 @@
+package Date10_06.CodeUp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_1030 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = bf.readLine();
+
+        long num = Long.parseLong(s);
+        System.out.println(s);
+    }
+}
