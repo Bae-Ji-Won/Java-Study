0911c07d6fbfb98728940ab4863d48aa87c2a151
qowl880 <qowhxk@naver.com>
2022-10-08 16:03:42 +0900

    [CodeUp] 1033번 문제

---
+++ git/Lion-Java/src/Date10_07/Project/CodeUp/No_1033.java
@@ -0,0 +1,17 @@
+package Date10_07.Project.CodeUp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_1033 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = bf.readLine();
+
+        int num = Integer.parseInt(s);
+
+        System.out.printf("%X",num);        // 대문자 x는 16진수 대문자로 출력
+    }
+}
