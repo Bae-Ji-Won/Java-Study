73f6ef407e20451613a7f0aab5fb979104181323
qowl880 <qowhxk@naver.com>
2022-10-08 17:16:20 +0900

    [CodeUp] 1040번 문제

---
+++ git/Lion-Java/src/Date10_07/Project/CodeUp/No_1040.java
@@ -0,0 +1,15 @@
+package Date10_07.Project.CodeUp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_1040 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(bf.readLine());
+
+        System.out.println(-(num));
+    }
+}
