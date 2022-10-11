0f14e162746f6284ab88e5ec71dfe4429e8bd486
Bae-Ji-Won <qowhxk@naver.com>
2022-10-11 23:28:01 +0900

    [CodeUp] 1047번 문제(비트 단위 시프트)

---
+++ git/Lion-Java/src/Date10_11/Algorithm/CodeUp/No_1047.java
@@ -0,0 +1,20 @@
+package Date10_11.Algorithm.CodeUp;
+
+import java.util.Scanner;
+
+public class No_1047 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num = scan.nextInt();
+
+        System.out.println(num<<1);
+
+        // 비트 단위 시프트 연산자
+        // ex. int a = 10;
+        //  printf("%d", a<<1); //10을 2배 한 값인 20 이 출력된다.
+        //  printf("%d", a>>1); //10을 반으로 나눈 값인 5 가 출력된다.
+        //  printf("%d", a<<2); //10을 4배 한 값인 40 이 출력된다.
+        //  printf("%d", a>>2); //10을 반으로 나눈 후 다시 반으로 나눈 값인 2 가 출력된다.
+    }
+}
