294baaac3da46d3fd3ba5a5f8709bac5e9bbabe6
Bae-Ji-Won <qowhxk@naver.com>
2022-10-11 23:27:39 +0900

    [CodeUp] 1044번 문제

---
+++ git/Lion-Java/src/Date10_11/Algorithm/CodeUp/No_1044.java
@@ -0,0 +1,13 @@
+package Date10_11.Algorithm.CodeUp;
+
+import java.util.Scanner;
+
+public class No_1044 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        long num =scan.nextInt();
+
+        System.out.println(num+1);
+    }
+}
