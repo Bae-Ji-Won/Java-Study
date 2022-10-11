9c0c3c40720f1b2c8534526887d1be1ffa37d748
Bae-Ji-Won <qowhxk@naver.com>
2022-10-11 23:27:34 +0900

    [CodeUp] 1043번 문제

--- git/Lion-Java/src/Date10_11/Algorithm/CodeUp/No_1043.java
+++ git/Lion-Java/src/Date10_11/Algorithm/CodeUp/No_1043.java
@@ -1,7 +1,16 @@
 package Date10_11.Algorithm.CodeUp;
 
+import java.util.Scanner;
+
 public class No_1043 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        String[] str = scan.nextLine().split(" ");
+
+        int num1 = Integer.parseInt(str[0]);
+        int num2 = Integer.parseInt(str[1]);
+
+        System.out.println(num1 % num2);
     }
 }
