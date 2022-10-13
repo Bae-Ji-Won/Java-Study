8acd2f78a63849afd06a42aa418b616da7be8f26
Bae-Ji-Won <qowhxk@naver.com>
2022-10-13 10:12:07 +0900

    [알고리즘] 버블정렬

--- git/Lion-Java/src/Date10_13/Algorithm/BubbleSort.java
+++ git/Lion-Java/src/Date10_13/Algorithm/BubbleSort.java
@@ -11,22 +11,25 @@ package Date10_13.Algorithm;
 import java.util.Arrays;
 public class BubbleSort {
 
-    public int[] sort(int[] arr){
-        for(int j = 1; j<arr.length; j++){
-            if(arr[j-1]>arr[j]){
-                int temp = arr[j];
-                arr[j] = arr[j-1];
-                arr[j-1] = temp;
+        public int[] sort(int[] arr){
+            for(int i=1; i<arr.length; i++) {
+                for (int j = 0 ; j < arr.length-i; j++) {
+                    if (arr[j] > arr[j+1]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+                System.out.println(Arrays.toString(arr));
             }
+            return arr;
         }
-        return arr;
-    }
 
-    public static void main(String[] args) {
-        int[] arr = {7,2,3,9,28,11};
-        BubbleSort b = new BubbleSort();
-        int[] result = b.sort(arr);
+        public static void main(String[] args) {
+            int[] arr = {7,2,3,9,28,11};
+            BubbleSort b = new BubbleSort();
+            int[] result = b.sort(arr);
 
-        System.out.println(Arrays.toString(result));
-    }
+            System.out.println(Arrays.toString(result));
+        }
 }
