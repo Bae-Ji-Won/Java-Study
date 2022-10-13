559890c9610ca887ff5cc8281257d8a75b9c3fb1
Bae-Ji-Won <qowhxk@naver.com>
2022-10-13 22:36:45 +0900

    [CodeUp] 1056번 문제

---
+++ git/Lion-Java/src/Date10_13/Algorithm/CodeUp/No_1056.java
@@ -0,0 +1,21 @@
+package Date10_13.Algorithm.CodeUp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_1056 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = br.readLine().split(" ");
+
+        int num1 = Integer.parseInt(str[0]);
+        int num2 = Integer.parseInt(str[1]);
+
+        if((num1 ==1 && num2 != 1)||(num1 !=1 && num2 == 1))
+            System.out.println("1");
+        else
+            System.out.println("0");
+    }
+}
