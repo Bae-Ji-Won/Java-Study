8fc867150d2bc11219be96096fe2d53dfe9dbd50
Bae-Ji-Won <qowhxk@naver.com>
2022-10-13 13:14:45 +0900

    Parser(""제거, address 추가)

--- lion/src/main/java/Date10_12/pasrser/HospitalParser.java
+++ lion/src/main/java/Date10_12/pasrser/HospitalParser.java
@@ -4,9 +4,14 @@ import Date10_12.domain.Hospital;
 
 // 클래스에서 alt+enter를 눌러 테스트를 할 수 있다.
 public class HospitalParser implements Parser<Hospital>{
+
+    private String replaceAll(String str){      // " " 제거
+        return  str.replaceAll("\"","");
+    }
+
     @Override
     public Hospital parse(String str) {
         String[] splitted = str.split(",");
-        return new Hospital(splitted[0]);
+        return new Hospital(replaceAll(splitted[0]),replaceAll(splitted[1]));
     }
 }
