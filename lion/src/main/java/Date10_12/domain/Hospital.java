cf71fad11e4955ecd8e2a1634bdf3d473c40f322
Bae-Ji-Won <qowhxk@naver.com>
2022-10-13 14:22:30 +0900

    Hospital(category,emergency,name 추가)

--- lion/src/main/java/Date10_12/domain/Hospital.java
+++ lion/src/main/java/Date10_12/domain/Hospital.java
@@ -1,5 +1,8 @@
 package Date10_12.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Hospital {
     private String id;      // [0]
     private String address; //주소  [1]
@@ -9,23 +12,8 @@ public class Hospital {
     private String subDivision; //세부분과  [10] 수정
     private int emergencyRoom; //응급 운영 현황    [6]
 
-    public Hospital(String id, String address, String category, String name, String subDivision, int emergencyRoom) {
-        this.id = id;
-        this.address = address;
-        this.category = category;
-        this.name = name;
-        this.subDivision = subDivision;
-        this.emergencyRoom = emergencyRoom;
-    }
-
-    public Hospital(String id, String address) {
-        this.id = id;
-        this.address = address;
-    }
-
-
     public Hospital(String id, String address, String category, int emergencyRoom,
-                    String name, String subDivision){
+                    String name){
         this.id = id;
         this.address = address;
         this.category = category;
@@ -33,6 +21,7 @@ public class Hospital {
         this.emergencyRoom = emergencyRoom;
         this.subDivision = subDivision;
         this.setDistrict();
+        this.setCategory();
     }
 
     public String getId() {
@@ -43,12 +32,46 @@ public class Hospital {
         return address;
     }
 
+
+    public void setDistrict() {
+        String[] address = this.address.split(" ");
+        this.district = address[0] + " " + address[1];
+    }
+
     public String getDistrict() {
         return district;
     }
 
+    public void setCategory() {      // 맵핑
+        Map<String,String> categorytmap = new HashMap<>();
+        categorytmap.put("A","종합병원");
+        categorytmap.put("B","병원");
+        categorytmap.put("C","의원");
+        categorytmap.put("D","요양병원");
+        categorytmap.put("E","한방병원");
+        categorytmap.put("G","한의원");
+        categorytmap.put("I","기타");
+        categorytmap.put("M","치과병원");
+        categorytmap.put("N","치과의원");
+        categorytmap.put("R","보건소");
+        categorytmap.put("W","기타(구급차)");
+
+        category = categorytmap.get(category);
+    }
+
     public String getCategory() {
         return category;
+    }
+
+    public String setEmergencyRoom() {
+        if(emergencyRoom == 1){
+            return "운영중";
+        }else
+            return "운영안함";
+    }
+
+    public String getEmergencyRoom() {
+        return setEmergencyRoom();
     }
 
     public String getName() {
@@ -59,15 +82,9 @@ public class Hospital {
         return subDivision;
     }
 
-    public int getEmergencyRoom() {
-        return emergencyRoom;
-    }
 
 
-    public void setDistrict() {
-        String[] address = this.address.split(" ");
-        this.district = address[0] + " " + address[1];
-    }
+
 
     @Override
     public String toString() {
