6f0ea94cb7afc009d389187452a9b191cd40eace
Bae-Ji-Won <qowhxk@naver.com>
2022-10-13 13:44:05 +0900

    HospitalParser(district 추가)

--- lion/src/main/java/Date10_12/pasrser/HospitalParser.java
+++ lion/src/main/java/Date10_12/pasrser/HospitalParser.java
@@ -5,13 +5,12 @@ import Date10_12.domain.Hospital;
 // 클래스에서 alt+enter를 눌러 테스트를 할 수 있다.
 public class HospitalParser implements Parser<Hospital>{
 
-    private String replaceAll(String str){      // " " 제거
-        return  str.replaceAll("\"","");
-    }
 
     @Override
     public Hospital parse(String str) {
+        str = str.replaceAll("\"","");  // "" 제거
         String[] splitted = str.split(",");
-        return new Hospital(replaceAll(splitted[0]),replaceAll(splitted[1]));
+
+        return new Hospital(splitted[0],splitted[1],splitted[2],Integer.parseInt(splitted[6]),splitted[4],splitted[5]);
     }
 }
