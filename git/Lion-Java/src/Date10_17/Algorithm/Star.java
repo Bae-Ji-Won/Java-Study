6ba422efa2769bcf1333c41c57a68df10cac9893
Bae-Ji-Won <qowhxk@naver.com>
2022-10-17 09:10:13 +0900

    직각삼각형 별찍기

---
+++ git/Lion-Java/src/Date10_17/Algorithm/Star.java
@@ -0,0 +1,14 @@
+package Date10_17.Algorithm;
+
+/* 알고리즘 문제
+
+    별찍기(직각 삼각형)
+ */
+public class Star {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
