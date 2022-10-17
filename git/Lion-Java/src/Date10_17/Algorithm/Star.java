1ac25ba9021db01ff88e9b1e45a4f0ad51a96905
Bae-Ji-Won <qowhxk@naver.com>
2022-10-17 09:20:44 +0900

    직각삼각형 별찍기

--- git/Lion-Java/src/Date10_17/Algorithm/Star.java
+++ git/Lion-Java/src/Date10_17/Algorithm/Star.java
@@ -14,13 +14,13 @@ public class Star {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        int num = Integer.parseInt(bf.readLine());
+        int num = Integer.parseInt(bf.readLine());      // 라인 개수 입력 받음
 
         input(num);
     }
 
     static void input(int num){
-        for(int i=0; i<num; i++){
+        for(int i=0; i<num; i++){           // 입력 받은 라인 개수 만큼 반복
             for(int j=0; j<i+1; j++) {
                 System.out.print("*");
             }
