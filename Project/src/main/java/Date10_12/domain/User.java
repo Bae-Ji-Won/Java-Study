a1092df8f077f141cf68036f6c14fbfb3652a06e
Bae-Ji-Won <qowhxk@naver.com>
2022-10-18 23:06:45 +0900

    DTO  (MYSQL 연동)

--- Project/src/main/java/Date10_12/domain/User.java
+++ Project/src/main/java/Date10_12/domain/User.java
@@ -1,4 +1,32 @@
 package Date10_12.domain;
 
 public class User {
+    private String id;
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String toString(){
+        return "id = "+this.id + " name = "+this.name+" password = "+this.password;
+    }
 }
