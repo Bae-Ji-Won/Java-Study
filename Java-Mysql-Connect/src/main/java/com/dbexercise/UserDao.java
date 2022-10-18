673a78961dd23baef67c4c3a9282cb12f0fa3b77
Bae-Ji-Won <qowhxk@naver.com>
2022-10-18 23:05:17 +0900

    DTO (MYSQL 연동)

--- Java-Mysql-Connect/src/main/java/com/dbexercise/UserDao.java
+++ Java-Mysql-Connect/src/main/java/com/dbexercise/UserDao.java
@@ -18,7 +18,7 @@ public class UserDao {
 
 
         Class.forName("com.mysql.cj.jdbc.Driver");  // 만약 DB드라이버가 2개 이상일때 어떤 DB에 데이터를 삽입할 것인지 구분하기 위해 사용함
-                                                             // 지금은 mysql 1개만 사용하기 때문에 없어도 됨 
+                                                             // 지금은 mysql 1개만 사용하기 때문에 없어도 됨
         Connection conn = DriverManager.getConnection(dbHost,dbUser,dbPassword);    // db 연결(호스트,이름,비밀번호)
         PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id,name,password) VALUES(?,?,?)");
         ps.setString(1,"4");        // mysql 테이블로 값 insert
@@ -32,23 +32,23 @@ public class UserDao {
         System.out.println("데이터가 insert 됬습니다.");
     }
 
-    public User select(String id) throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");  // 만약 DB드라이버가 2개 이상일때 어떤 DB에 데이터를 삽입할 것인지 구분하기 위해 사용함
-        // 지금은 mysql 1개만 사용하기 때문에 없어도 됨
-        Connection conn = DriverManager.getConnection(dbHost,dbUser,dbPassword);    // db 연결(호스트,이름,비밀번호)
-        PreparedStatement ps = conn.prepareStatement("SELECT id,name,password FROM users WHERE id = ?");
-        ps.setString(1,id);     // id는 get(String id)로 받은 id
-        ResultSet rs = ps.executeQuery();   // 쿼리문을 저장함 insert문과 달리 excuteQuery() 사용
-                                            // rs에는 쿼리 실행 결과가 담겨져 있다. (select * from users where id = 1;)
-        rs.next();
+        public User select(String id) throws SQLException, ClassNotFoundException {
+            Class.forName("com.mysql.cj.jdbc.Driver");  // 만약 DB드라이버가 2개 이상일때 어떤 DB에 데이터를 삽입할 것인지 구분하기 위해 사용함
+            // 지금은 mysql 1개만 사용하기 때문에 없어도 됨
+            Connection conn = DriverManager.getConnection(dbHost,dbUser,dbPassword);    // db 연결(호스트,이름,비밀번호)
+            PreparedStatement ps = conn.prepareStatement("SELECT id,name,password FROM users WHERE id = ?");
+            ps.setString(1,id);     // id는 get(String id)로 받은 id
+            ResultSet rs = ps.executeQuery();   // 쿼리문을 저장함 insert문과 달리 excuteQuery() 사용
+                                                // rs에는 쿼리 실행 결과가 담겨져 있다. (select * from users where id = 1;)
+            rs.next();
 
-        // User 생성자를 통해 쿼리문에 id값을 넣어 찾은 id, name,password 값을 저장한다.
-        User user = new User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
-        rs.close();
-        ps.close();
-        conn.close();
-        return user;
-    }
+            // User 생성자를 통해 쿼리문에 id값을 넣어 찾은 id, name,password 값을 저장한다.
+            User user = new User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
+            rs.close();
+            ps.close();
+            conn.close();
+            return user;
+        }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
