f42a994e67add8d95a871196351f21e2a0a55be9
Bae-Ji-Won <qowhxk@naver.com>
2022-10-20 11:00:16 +0900

    DAO-Factory(spring 추가)

--- Project/src/main/java/Date10_20/dao/Factory/UserDaoFactory.java
+++ Project/src/main/java/Date10_20/dao/Factory/UserDaoFactory.java
@@ -5,17 +5,20 @@ package Date10_20.dao.Factory;
 // 즉, 인터페이스로 DB별 연결 설정을 나눠놨는데 여기서 메서드별로 다시 DB별로 나누어
 // UserDaoInterface로 해당 DB 연결 설정 값을 보냄
 // (4번째 방식)
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
-    public static void main(String[] args) {
 
-    }
-
+    @Bean
     public UserDaoInterface awsUserDao(){
         AWSConnectionMaker awsConnectionMaker = new AWSConnectionMaker();
         UserDaoInterface userDao = new UserDaoInterface(awsConnectionMaker);
         return userDao;
     }
 
+    @Bean
     public UserDaoInterface localUserDao(){
         UserDaoInterface userDao = new UserDaoInterface(new LocalConnectionMaker());
         return userDao;
