62d888ea642b1505dff7b1bba03a4d57d32dd272
Bae-Ji-Won <qowhxk@naver.com>
2022-10-23 23:35:58 +0900

    TDD

--- Project/src/test/java/Date10_20/Factory/UserDaoTest.java
+++ Project/src/test/java/Date10_20/Factory/UserDaoTest.java
@@ -5,6 +5,7 @@ import Date10_20.dao.Factory.UserDao;
 import Date10_20.domain.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)  // spring에서 테스트 하기 위한 설정
 @ContextConfiguration(classes = UserDaoFactory.class)
-class UserDaoTest {
+class UserDaoFactoryTest {
                     // 싱글톤을 위해 Autowired 사용
     @Autowired      // 새로운 객체를 사용하지 않고 이전에 사용했던 객체의 주소를 그대로 사용한다는 설정
                     // new 객체 생성을 한번만 사용함(고정값)
@@ -46,19 +47,19 @@ class UserDaoTest {
         Assertions.assertThat(0).isEqualTo(userDao.getCount());
 
         String id = "3";
-        userDao.add(user1);
+        userDao.add(user3);
         Assertions.assertThat(1).isEqualTo(userDao.getCount());
 
         User user = userDao.select(id);
 
-        Assertions.assertThat("Spring").isEqualTo(user.getName());
+        Assertions.assertThat("세종").isEqualTo(user.getName());
    //     assertEquals("Spring",user.getName());
     }
     @Test
         //.count test만들기
     void count() throws SQLException, ClassNotFoundException {
 
-        UserDao userDao = context.getBean("AwsUserDao", UserDao.class);
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
         //.deleteAll() 오류 검증
         userDao.deleteAll();
         assertEquals(0, userDao.getCount());
@@ -70,10 +71,15 @@ class UserDaoTest {
         userDao.add(user3);
         assertEquals(3, userDao.getCount());
     }
+
+
     @Test
+    @DisplayName("")
     void findById() {
         assertThrows(EmptyResultDataAccessException.class, ()-> {
             userDao.select("30"); // 데이터가 없을때 ResultSet이 빈 경우 null로 오류가 난다.
         });
     }
+
+
 }
