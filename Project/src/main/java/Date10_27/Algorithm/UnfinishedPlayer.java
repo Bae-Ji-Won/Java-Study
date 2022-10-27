a20ba26e0f9a2b1771702e0ea61cea30ad1a70ac
Bae-Ji-Won <qowhxk@naver.com>
2022-10-27 10:54:36 +0900

    [알고리즘] Hash(2)

--- Project/src/main/java/Date10_27/Algorithm/UnfinishedPlayer.java
+++ Project/src/main/java/Date10_27/Algorithm/UnfinishedPlayer.java
@@ -1,7 +1,6 @@
 package Date10_27.Algorithm;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 /* 프로그래머스 완주하지 못한 선수 문제
@@ -11,29 +10,35 @@ import java.util.Map;
  */
 public class UnfinishedPlayer {
     public String solution(String[] participant, String[] completion) {
-        Map<String,Integer> memo = new HashMap<>();
+        Map<String, Integer> memo = new HashMap<>();
 
-        for(int i=0; i<participant.length; i++){
+        for (int i = 0; i < participant.length; i++) {
             String key = participant[i];        // key에는 participant배열의 값을 넣음
-            memo.put(key,1);            // 명단이 들어간 자리에는 1의 값을 넣어줌
-                                        // ex) {"홍길동","이순신"} 배열 대입
-                                        // ["홍길동",1] ,["이순신",1]
-        }
 
-        for(int i=0; i<completion.length; i++){
-            String key = completion[i];
-            if(memo.get(key) == 1){        // 입력받은 키의 값이 1일때
-                memo.remove(key);           // 키의 데이터를 map에서 삭제한다
+            if (!memo.containsKey(key)) {     // 동명이인 구분을 위해 이미 같은 이름의 키가 존재하지 않다면
+                memo.put(key, 0);            // [key,0] 대입
             }
+
+            memo.put(key, memo.get(key) + 1);
+            // 명단이 들어간 자리에는 +1의 값을 넣어줌
+            // 즉, 새로 생긴 값이라면 0+1 , 동명이인이라 이미 있는 값이라면 1+1
+            // ex) {"홍길동","이순신","홍길동"} 배열 대입
+            // ["홍길동",2] ,["이순신",1]
         }
 
-        Iterator<String> iterator = memo.keySet().iterator();
-        String key = null;
-
-        while(iterator.hasNext()){
-            key = iterator.next();
+        for (int i = 0; i < completion.length; i++) {
+            String key = completion[i];
+            memo.put(key, memo.get(key) - 1);      // key의 값에서 -1을 한다.
+            // {"홍길동","이순신"} 이라면
+            // ["홍길동",1] ,["이순신",0]
         }
 
-        return key;
+        String person = null;
+
+        for (String key : memo.keySet()) {
+            if (memo.get(key) == 1)
+                person = key;
+        }
+        return person;
     }
 }
