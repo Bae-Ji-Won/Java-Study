7a22b6b737e48c84eae671482f7de10e432c2eba
Bae Ji Won <qowhxk@naver.com>
2022-11-11 14:43:07 +0900

    선택정렬(DiFunction, DiPredicate 사용)

--- Project/src/main/java/Date11_09/Algorithm/SelectionSort3.java
+++ Project/src/main/java/Date11_09/Algorithm/SelectionSort3.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 
 /*  /*  선택정렬(3) - ByFunction 사용해서 구현해보기
      index = 0번부터 제일 작은 값으로 채워 나감
@@ -15,6 +16,7 @@ import java.util.function.BiFunction;
 public class SelectionSort3 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    // BiFunction을 사용한 풀이
     public int[] solution(int[] arr, BiFunction<Integer,Integer,Boolean> stmt){
         int[] result = arr;
 
@@ -22,6 +24,24 @@ public class SelectionSort3 {
             int minidx = i;
             for(int j=i; j<arr.length; j++){
                 if(stmt.apply(arr[minidx],arr[j]))        // apply 결과값이 참일때 동작함
+                    minidx = j;
+            }
+            int temp = result[i];
+            result[i] = result[minidx];
+            result[minidx] = temp;
+        }
+
+        return result;
+    }
+
+    // BiPredicate을 사용한 풀이 BiPredicate도 <Integer,Integer,Boolean>을 사용해야하지만 default값이 boolean이기 때문에 Boolean은 생략 가능
+    public int[] solution2(int[] arr, BiPredicate<Integer,Integer> stmt){
+        int[] result = arr;
+
+        for(int i=0; i<arr.length; i++){
+            int minidx = i;
+            for(int j=i; j<arr.length; j++){
+                if(stmt.test(arr[minidx],arr[j]))        // apply 결과값이 참일때 동작함
                     minidx = j;
             }
             int temp = result[i];
@@ -45,9 +65,11 @@ public class SelectionSort3 {
         }
 
         BiFunction<Integer,Integer,Boolean> biFunction = (a,b) -> a>b;
-        BiFunction<Integer,Integer,Boolean> biFunction2 = (a,b) -> a<b;
+        BiPredicate<Integer,Integer> biPredicate = (a,b) -> a<b;
+
+     //   BiFunction<Integer,Integer,Boolean> biFunction2 = (a,b) -> a<b;
 
         System.out.println(Arrays.toString(s.solution(arr,biFunction)));
-        System.out.println(Arrays.toString(s.solution(arr,biFunction2)));
+        System.out.println(Arrays.toString(s.solution2(arr,biPredicate)));
     }
 }
